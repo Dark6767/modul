4124996eb45a2879f2c8aa178bbc320bfc07bdc8
Dark6767 <48690111+Dark6767@users.noreply.github.com>
2019-03-31 20:33:39 +0300

    Delete Module_url.py

--- Module_url.py
+++
@@ -1,7 +0,0 @@
-urls=[['','']]
-def input_url(name_url,srt_url):
-    urls.append([name_url,srt_url])
-def search_url(shrt_url):
-    for i in urls:
-        if i[1]==shrt_url:
-            print('Полная ссылка>>'+i[0])
