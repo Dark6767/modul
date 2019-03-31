f5072903db4134d3df224021375bc6204291b569
Dark6767 <48690111+Dark6767@users.noreply.github.com>
2019-03-31 20:33:47 +0300

    Delete ex3.py

--- ex3.py
+++
@@ -1,7 +0,0 @@
-import Module_url
-def main():
-    Module_url.input_url(input('Введите полную ссылку>'),input('Введите сокращенную ссылку>'))
-    Module_url.input_url(input('Введите полную ссылку>'),input('Введите сокращенную ссылку>'))
-    Module_url.search_url(input('Введите сокращенную ссылку для получения полной>'))
-if __name__ == '__main__':
-    main()
