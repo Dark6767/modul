bf3e7d7eb7a2ddc2a1005d4f05cae45817950e42
Dark6767 <48690111+Dark6767@users.noreply.github.com>
2019-03-31 19:33:23 +0300

    Add files via upload

---
+++ My_module.py
@@ -0,0 +1,8 @@
+import random
+number1=11
+number2=22
+number3=33
+number4=44
+number5=55
+def search(number):
+    return random.randint(0,number)
---
+++ ex1.py
@@ -0,0 +1,4 @@
+import My_module
+print(str(My_module.number1))
+print(str(My_module.search(20)))
+print(str(My_module.number5))
